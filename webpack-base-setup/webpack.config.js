5269cf8665ad2c49cf929e36f5b11b60969d4f44
Yingru Fang <fyr1116@gmail.com>
2021-02-07 22:59:43 -0600

    add extract-text-webpack-plugin

--- webpack-base-setup/webpack.config.js
+++ webpack-base-setup/webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // extracts CSS into separate files
 
 const config = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: 'build/'
   },
   module: {
     rules: [
@@ -13,11 +15,15 @@ const config = {
         test: /\.js$/ //format in regular expression
       },
       {
-        use: ['style-loader', 'css-loader'],
+        // use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
         test: /\.css$/
       }
     ]
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin()
+  ]
 };
 
 module.exports = config;
